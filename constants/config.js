c191ec40b013017fdf9aae43e90fcd54fcf11589
Nikunj Goel <72417259+nikunj-oss@users.noreply.github.com>
2024-09-22 16:31:17 +0530

    modified cors

--- constants/config.js
+++ constants/config.js
@@ -1,9 +1,13 @@
-const corsOptions={
-    
-    origin:["http://localhost:5173","http://localhost:4173",process.env.CLIENT_URL],
-    credentials:true //we can send headers
-    
-}
+const corsOptions = {
+    origin: [
+      "http://localhost:5173", 
+      "http://localhost:4173", 
+      process.env.CLIENT_URL || "https://chat-app-frontend-9o27j5oey-nikunj-goels-projects.vercel.app"
+    ],
+    credentials: true, // Allows cookies and authentication headers to be sent
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Ensure the correct HTTP methods are allowed
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow custom headers like Authorization
+  };
 
 const SYNC_TOKEN="sync-token"
 
